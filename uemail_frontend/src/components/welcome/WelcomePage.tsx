2bdbf75a09bb1375ca2dc20de8c066442c712050
arpitparmar <arpitparmar5739@gmail.com>
2018-03-09 01:54:14 +0530

    Added support for css files, bootstrap and started working on the welcome page of the website. Also changed the webpack file from typescript to javascript for developer mode option.

---
+++ uemail_frontend/src/components/welcome/WelcomePage.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class Welcome extends React.Component<{}, {}> {
+  public render() {
+    return (
+      <div>
+        Welcome!
+      </div>
+    );
+  }
+}
+
+export default Welcome;
